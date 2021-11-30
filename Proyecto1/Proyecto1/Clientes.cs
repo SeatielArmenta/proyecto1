f1c664d7ef549ab646814d80deb8dcd740c13808
Zero F <fleischercarbajal@gmail.com>
2021-11-30 10:21:42 -0700

    cambios

---
+++ Proyecto1/Proyecto1/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1
+{
+    class Clientes : Cuentas
+    {
+        string rFC, nombre, apellidos, telefono, direccion;
+
+        public string RFC { get => rFC; set => rFC = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+    }
+}
---
+++ Proyecto1/Proyecto1/Cuentas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1
+{
+    public class Cuentas
+    {
+        int numeroDeCuenta, numeroDeCliente;
+        double saldo;
+
+        public int NumeroDeCuenta { get => numeroDeCuenta; set => numeroDeCuenta = value; }
+        public int NumeroDeCliente { get => numeroDeCliente; set => numeroDeCliente = value; }
+        public double Saldo { get => saldo; set => saldo = value; }
+    }
+}
